0541cf820597bdf20a9953d9515fb8476fc03300
Jose Carlos <josecarlos05032000@gmail.com>
2018-08-21 11:59:21 -0600

    Objetivo Alcanzado

--- Excel_to_DB/src/excel_to_db/Conn_Database.java
+++ Excel_to_DB/src/excel_to_db/Conn_Database.java
@@ -4,43 +4,40 @@ package excel_to_db;
  *
  * @author josec
  */
-
+//imports
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Date;
 
 public class Conn_Database {
+    
     private Connection connect = null;
+    
     public void connDataBase() throws Exception {
+        
         try {
-            // This will load the MySQL driver, each DB has its own driver
             Class.forName("com.mysql.cj.jdbc.Driver");
-            // Setup the connection with the DB
             connect = DriverManager
                     .getConnection("jdbc:mysql://localhost/db_prueba?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&user=root&password=rootadmin");
+            System.out.println("¡Conection Successfull!");  
             
-            System.out.println("¡Conection Successfull!");
-                       
         } catch (Exception e) {
             throw e;
-        } finally {
-            close();
         }
-
+        
+    }
+    
+    public boolean ejecutar(String sentencia) throws SQLException {
+        return connect.createStatement().execute(sentencia);
     }
 
-    // You need to close the resultSet
-    private void close() {
+    public void close() {
         try {
             if (connect != null) {
                 connect.close();
             }
         } catch (Exception e) {
-
+            System.out.println(e);
         }
     }
 }  
--- Excel_to_DB/src/excel_to_db/Excel_to_DB.java
+++ Excel_to_DB/src/excel_to_db/Excel_to_DB.java
@@ -1,5 +1,6 @@
 package excel_to_db;
 
+//imports
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
@@ -8,37 +9,27 @@ import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
-
-
-
 /**
  *
  * @author josec
  */
 
-//Imports
-
 public class Excel_to_DB {
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args){
         
         try {
             Conn_Database con = new Conn_Database();
             con.connDataBase();
             
-            InputStream myFile = new FileInputStream(new File("C:\\Users\\josec\\Desktop\\DB_Excel\\Excel_to_DB\\movies.xls"));
+            File carpeta = new File("C:\\Users\\josec\\Desktop\\DB_Excel\\Excel_to_DB\\movies.xls");
+            
+            InputStream myFile = new FileInputStream(carpeta);
             HSSFWorkbook wb = new HSSFWorkbook(myFile);
             HSSFSheet sheet = wb.getSheet("movies");
 
             HSSFCell cell;
             HSSFRow row;
 
-            System.out.println("Cantidad de loops");
-
-            System.out.println("" + sheet.getLastRowNum());
-            
             for (int i = 0; i < sheet.getLastRowNum() + 1; i++) {
                 row = sheet.getRow(i);
                 String strCell = "";
@@ -48,19 +39,21 @@ public class Excel_to_DB {
                 }
                 String[] partirConjunto = strCell.split("::");
                 String[] separarCadena = partirConjunto[1].split("\\(");
-                String year= "";
+                String year;
                 if(separarCadena.length > 2){
                    separarCadena[0] += ("(" + separarCadena[1]);
                    year = separarCadena[2].split("\\)")[0];
                 } else {
                    year = separarCadena[1].split("\\)")[0];
                 }
+                con.ejecutar("INSERT INTO movies_tbl (_id, _name, _year, _category) values ('" + partirConjunto[0] + "', \"" + separarCadena[0]  + "\", \"" + year + "\", \"" + partirConjunto[2] +  "\")");
                 System.out.println("{ id: " + partirConjunto[0] + " nombre: " + separarCadena[0] + " año: " + year + " categoria: " + partirConjunto[2] + " }");
             }
-            System.out.println("Finalizado");
+            con.close();
+            System.out.println("");
+            System.out.println("¡ " + sheet.getLastRowNum() +" Datos Impresos Correctamente!");
 
         } catch (Exception e) {
-            // TODO: handle exception
             System.out.println(e.getMessage());
         }
     }
